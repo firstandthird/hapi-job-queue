8bbc2a8048fa34fc21fe41e35363e0ef06edb251
Troy Whiteley <dawnerd@users.noreply.github.com>
2015-04-04 00:19:04 -0700

    More work towards running jobs.

--- test/jobs.test.js
+++ test/jobs.test.js
@@ -1,3 +1,5 @@
+var MongoClient = require('mongodb').MongoClient;
+
 var Code = require('code');
 var Lab = require('lab');
 var lab = exports.lab = Lab.script();
@@ -9,39 +11,53 @@ var before = lab.before;
 var after = lab.after;
 var expect = Code.expect;
 
-describe('job queue', function() {
-  var server;
-  var plugin;
-  var output = null;
+var mongoUrl = 'mongodb://localhost:27017/hapi-job-queue-test';
 
-  before(function(done) {
-    server = new Hapi.Server();
-    server.connection({port: 3000});
+var server;
+var plugin;
+var output = null;
 
-    server.register([
-      { register: require('../'), options: {
-        connectionUrl: 'mongodb://localhost:27017/hapi-job-queue-test',
-        jobs: [
-          {
-            name: 'test-job',
-            enabled: false,
-            method: function(data, cb) {
-              output = 'ran';
-              cb();
-            },
-            tasks: [
-              {
-                something: 'some data'
-              }
-            ]
-          }
-        ]
-      } }
-    ], function() {
-      plugin = server.plugins.jobs;
-      done();
+before(function(done) {
+  server = new Hapi.Server();
+  server.connection({port: 3000});
+
+  MongoClient.connect(mongoUrl, function(err, db) {
+    if (err) {
+      return next(err);
+    }
+
+    db.dropDatabase(function(err) {
+
+      server.register([
+        { register: require('../'), options: {
+          connectionUrl: mongoUrl,
+          jobs: [
+            {
+              name: 'test-job',
+              enabled: false,
+              schedule: 'every 5 seconds',
+              method: function(data, cb) {
+                output = 'ran';
+                cb();
+              },
+              tasks: [
+                {
+                  something: 'some data'
+                }
+              ]
+            }
+          ]
+        } }
+      ], function() {
+        plugin = server.plugins.jobs;
+        done();
+      });
+
     });
   });
+});
+
+describe('job queue', function() {
 
   describe('setup', function() {
     it('should expose db connection', function(done) {
@@ -61,27 +77,34 @@ describe('job queue', function() {
 
     it('should initialize jobs', function(done) {
       plugin.collection.find({name: 'test-job'}).toArray(function(err, jobs) {
+        expect(err).to.not.exist();
+
         expect(jobs.length).to.equal(1);
 
         var job = jobs[0];
 
         expect(job.name).to.equal('test-job');
-        // expect(job.nextRun).to.equal(+new Date());
         expect(job.locked).to.equal(false);
         expect(job.lastRun).to.equal(undefined);
         expect(job.timeToRun).to.equal(undefined);
         expect(job.group).to.deep.equal(['test-job']);
         expect(job.tasks).to.deep.equal([{something: 'some data'}]);
+        expect(job.enabled).to.equal(false);
+        expect(job.nextRun).to.exist();
+
         done();
       });
     });
   });
 
   describe('methods', function() {
-    it.skip('should add a job', function(done) {
-      plugin.methods.add('test-job2', {
+    it('should add a job', function(done) {
+      plugin.add({
+        name: 'test-job2',
         enabled: true,
         group: 'group1',
+        cron: '* 15 10 ? * *',
+        cronSeconds: true,
         method: function(cb) {
           output = 'ran2';
           cb();
@@ -89,19 +112,19 @@ describe('job queue', function() {
       }, function(err) {
         expect(err).to.not.exist();
 
-        plugin.collection.find({name: 'test-job2'}, function(err, jobs) {
+        plugin.collection.find({name: 'test-job2'}).toArray(function(err, jobs) {
           expect(jobs.length).to.equal(1);
 
           var job = jobs[0];
 
           expect(job.name).to.equal('test-job2');
-          expect(job.nextRun).to.equal(+new Date());
           expect(job.locked).to.equal(false);
-          expect(job.enabled).to.equal(true);
           expect(job.lastRun).to.equal(undefined);
           expect(job.timeToRun).to.equal(undefined);
-          expect(job.group).to.equal('group1');
-          expect(job.status).to.equal('pending');
+          expect(job.group).to.deep.equal(['group1']);
+          expect(job.tasks).to.equal(null);
+          expect(job.enabled).to.equal(true);
+          expect(job.nextRun).to.exist();
 
           done();
         });
@@ -109,17 +132,18 @@ describe('job queue', function() {
       });
     });
 
-    it.skip('should only add a job once', function(done) {
-      plugin.methods.add('test-job', {
+    it('should only add a job once', function(done) {
+      plugin.add({
+        name: 'test-job',
         enabled: false,
         method: function(cb) {
           output = 'ran';
           cb();
         }
       }, function(err) {
-        expect(err).to.equal(new Error('Job already loaded'));
+        expect(err).to.deep.equal(new Error('Job already loaded'));
 
-        plugin.collection.find({name: 'test-job'}, function(err, jobs) {
+        plugin.collection.find({name: 'test-job'}).toArray(function(err, jobs) {
           expect(jobs.length).to.equal(1);
           done();
         });
@@ -127,31 +151,85 @@ describe('job queue', function() {
       });
     });
 
-    it.skip('should add job to job object', function(done) {
-      expect(plugin.jobs['test-job'].enabled).to.equal(false);
+    it('should add job to job object', function(done) {
+      expect(plugin.jobs['test-job']).to.exist();
       expect(typeof plugin.jobs['test-job'].method).to.equal('function');
       done();
     });
 
-    it.skip('should enable a job', function(done) {
-      plugin.methods.enable('test-job', function(err) {
+    it('should check if a job is valid before enabling', function(done) {
+      plugin.enable('fake-job', function(err) {
+        expect(err).to.deep.equal(new Error('Job doesn\'t exist'));
+        done();
+      });
+    });
+
+    it('should enable a job', function(done) {
+      plugin.enable('test-job', function(err) {
         expect(err).to.not.exist();
 
-        plugin.collection.find({name: 'test-job'}, function(err, jobs) {
+        plugin.collection.find({name: 'test-job'}).toArray(function(err, jobs) {
+
+          expect(err).to.not.exist();
+          expect(jobs.length).to.equal(1);
+
           expect(jobs[0].enabled).to.equal(true);
-          expect(plugin.jobs['test-job'].enabled).to.equal(true);
+
           done();
         });
       });
     });
 
-    it.skip('should disable a job', function(done) {
-      plugin.methods.disable('test-job', function(err) {
+    it('should check if a job is valid before disabling', function(done) {
+      plugin.disable('fake-job', function(err) {
+        expect(err).to.deep.equal(new Error('Job doesn\'t exist'));
+        done();
+      });
+    });
+
+    it('should disable a job', function(done) {
+      plugin.disable('test-job', function(err) {
         expect(err).to.not.exist();
 
-        plugin.collection.find({name: 'test-job'}, function(err, jobs) {
+        plugin.collection.find({name: 'test-job'}).toArray(function(err, jobs) {
+
+          expect(err).to.not.exist();
+          expect(jobs.length).to.equal(1);
+
           expect(jobs[0].enabled).to.equal(false);
-          expect(plugin.jobs['test-job'].enabled).to.equal(false);
+
+          done();
+        });
+      });
+    });
+
+    it('should enable a group', function(done) {
+      plugin.enableGroup('test-job', function(err) {
+        expect(err).to.not.exist();
+
+        plugin.collection.find({group: 'test-job'}).toArray(function(err, jobs) {
+
+          expect(err).to.not.exist();
+          expect(jobs.length).to.equal(1);
+
+          expect(jobs[0].enabled).to.equal(true);
+
+          done();
+        });
+      });
+    });
+
+    it('should disable a group', function(done) {
+      plugin.disableGroup('test-job', function(err) {
+        expect(err).to.not.exist();
+
+        plugin.collection.find({group: 'test-job'}).toArray(function(err, jobs) {
+
+          expect(err).to.not.exist();
+          expect(jobs.length).to.equal(1);
+
+          expect(jobs[0].enabled).to.equal(false);
+
           done();
         });
       });
@@ -171,10 +249,6 @@ describe('job queue', function() {
     });
 
     it.skip('should run a group of jobs right away', function(done) {
-      done();
-    });
-
-    it.skip('should group jobs by name', function(done) {
       done();
     });
   });
